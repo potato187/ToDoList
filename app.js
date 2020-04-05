490c72d3dfa1d2ac3873ecebf651f87a15057eac
Potato187 <tai.duongwdev@gmail.com>
2020-04-05 16:39:00 +0700

    Set template and folder public

--- app.js
+++ app.js
@@ -2,3 +2,16 @@ const express = require('express');
 
 
 const PORT = 3000;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static('public'));
+app.set('views engine', 'pug');
+app.set('views','./views');
+
+
+app.listen(PORT, () =>{
+    console.log(`http://localhost${3000}`);
+})
