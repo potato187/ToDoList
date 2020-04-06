f471be5b166d787f8d907e6f4317864a302ec881
Potato187 <tai.duongwdev@gmail.com>
2020-04-06 13:39:43 +0700

    Set current date

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const express = require('express');
-
+const lowDb   = require('lowdb');
+const low = require('lowdb')
+const FileSync = require('lowdb/adapters/FileSync')
+const adapter = new FileSync('db.json')
+const db = low(adapter)
 
 const PORT = 3000;
 
@@ -8,10 +12,17 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(express.static('public'));
-app.set('views engine', 'pug');
+app.set('view engine', 'pug');
 app.set('views','./views');
 
 
-app.listen(PORT, () =>{
-    console.log(`http://localhost${3000}`);
+app.get('/', (req, res) =>{
+    res.render('index',{
+        date : new Date().toJSON().slice(0,10).replace(/-/g,'/')
+    });
 })
+
+
+app.listen(PORT, () =>{
+    console.log(`http://localhost:${3000}`);
+}) 
